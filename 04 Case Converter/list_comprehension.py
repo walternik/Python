f8352b9ee179e2ad52cc18dfea568acf94c67666
Walter Melnik <walter.melnik@gmail.com>
2023-12-24 16:58:40 -0300

    Converter Case

---
+++ 04 Case Converter/list_comprehension.py
@@ -0,0 +1,16 @@
+# freeCodeCamp project
+# Convert case with list comprehension
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = [
+        '_' + char.lower() if char.isupper()
+        else char
+        for char in pascal_or_camel_cased_string
+    ]
+
+    return ''.join(snake_cased_char_list).strip('_')
+
+def main():
+    print(convert_to_snake_case('IAmAPascalCasedString'))
+	
+if __name__ == '__main__':
+    main()
