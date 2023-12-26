4e797a24864d2f20bc860d04f5c26e76af207a41
Walter Melnik <walter.melnik@gmail.com>
2023-12-26 15:46:52 -0300

    Password Generator

---
+++ 05 Password Generator/simple.py
@@ -0,0 +1,24 @@
+# Password generator
+# freeCodeCamp project
+import secrets
+import string
+
+
+def generate_password(length):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    password = ''
+    # Generate password
+    for _ in range(length):
+        password += secrets.choice(all_characters)
+        
+    return password
+
+new_password = generate_password(8)
+print(new_password)
