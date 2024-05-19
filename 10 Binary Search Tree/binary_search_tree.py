e46928b090cb912944652e182aac855cb5abdcda
Walter Melnik <walter.melnik@gmail.com>
2024-05-19 11:03:05 -0300

    Binary Search Tree

--- 10 Binary Search Tree/binary_search_tree.py
+++ 10 Binary Search Tree/binary_search_tree.py
@@ -16,9 +16,6 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, key):
-        self.root = self._insert(self.root, key)
-
     def _insert(self, node, key):
         if node is None:
             return TreeNode(key)
@@ -28,8 +25,8 @@ class BinarySearchTree:
             node.right = self._insert(node.right, key)
         return node
 
-    def search(self, key):
-        return self._search(self.root, key)
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
 
     def _search(self, node, key):
         if node is None or node.key == key:
@@ -37,36 +34,37 @@ class BinarySearchTree:
         if key < node.key:
             return self._search(node.left, key)
         return self._search(node.right, key)
-
-    def delete(self, key):
-        self.root = self._delete(self.root, key)
+    
+    def search(self, key):
+        # the search starts from the root
+        return self._search(self.root, key)
 
     def _delete(self, node, key):
+        # conditions for one or None children
         if node is None:
             return node
         if key < node.key:
             node.left = self._delete(node.left, key)
         elif key > node.key:
             node.right = self._delete(node.right, key)
+        # two children
         else:
             if node.left is None:
                 return node.right
             elif node.right is None:
                 return node.left
-
+            # the node has both left and right children
             node.key = self._min_value(node.right)
             node.right = self._delete(node.right, node.key)
         return node
+    
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
 
     def _min_value(self, node):
         while node.left is not None:
             node = node.left
         return node.key
-
-    def inorder_traversal(self):
-        result = []
-        self._inorder_traversal(self.root, result)
-        return result
 
     def _inorder_traversal(self, node, result):
         if node:
@@ -74,6 +72,10 @@ class BinarySearchTree:
             result.append(node.key)
             self._inorder_traversal(node.right, result)
 
+    def inorder_traversal(self):
+        result = []
+        self._inorder_traversal(self.root, result)
+        return result
 
 bst = BinarySearchTree()
 nodes = [50, 30, 20, 40, 70, 60, 80]
@@ -81,9 +83,9 @@ nodes = [50, 30, 20, 40, 70, 60, 80]
 for node in nodes:
     bst.insert(node)
 #print(bst.root)
+print("Search for 40:", bst.search(40))
 print("Inorder traversal:", bst.inorder_traversal())
-print("Search for 40:", bst.search(40))
 bst.delete(40)
-print("Inorder traversal after deleting 40:", bst.inorder_traversal())
 print("Search for 40:", bst.search(40))
+print("Inorder traversal after deleting 40:", bst.inorder_traversal())
 #print(bst.root)
